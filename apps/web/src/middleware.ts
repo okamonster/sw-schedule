dd10f822bf037c456e070be95c4538e7fb80ca17
okamonster <okamonster0615@gmail.com>
2025-06-25 22:04:10 +0900

    feat: 保護されたパスに設定ページを追加し、認証チェックのロジックを改善

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import { auth } from '@/auth';
 
-const protectedPaths = ['/entry', '/profile'];
+const protectedPaths = ['/entry', '/profile', '/setting'];
 
 // biome-ignore lint/suspicious/noExplicitAny: <req>
 export default auth((req: any) => {
   const reqUrl = new URL(req.url);
-  if (!req.auth && protectedPaths.includes(reqUrl.pathname)) {
+  if (!req.auth && protectedPaths.some((path) => reqUrl.pathname.startsWith(path))) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 });
