265784bb79fa42d7240429ec1de08fd11f3bff2b
okamonster <okamonster0615@gmail.com>
2025-06-25 20:06:56 +0900

    feat: 認証機能を改善し、Googleログインの処理を強化。保護されたパスへのアクセス制御を追加し、セッション管理を最適化。

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,16 @@
+import { NextResponse } from 'next/server';
 import { auth } from '@/auth';
 
-export default auth;
+const protectedPaths = ['/entry', '/profile'];
+
+// biome-ignore lint/suspicious/noExplicitAny: <req>
+export default auth((req: any) => {
+  const reqUrl = new URL(req.url);
+  if (!req.auth && protectedPaths.includes(reqUrl.pathname)) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+});
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
