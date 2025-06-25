7a8aa21c0608f9e19c4374fa0ec7d72b834710b9
okamonster <okamonster0615@gmail.com>
2025-06-25 20:38:02 +0900

    feat: プロフィール編集ページからローディングオーバーレイとサスペンスを削除し、ユーザー情報を直接フォームに表示するように変更

--- apps/web/src/app/setting/profile/page.tsx
+++ apps/web/src/app/setting/profile/page.tsx
@@ -1,16 +1,8 @@
-import { LoadingOverlay } from '@mantine/core';
-import { Suspense } from 'react';
 import { EditProfileForm } from '@/features/profile/components/EditProfileForm';
 import { getCurrentUser } from '@/service/user';
 
 export default async function EditProfilePage() {
   const user = await getCurrentUser();
 
-  console.log(user);
-
-  return (
-    <Suspense fallback={<LoadingOverlay visible={true} />}>
-      <EditProfileForm profile={user?.profile} />
-    </Suspense>
-  );
+  return <EditProfileForm profile={user?.profile} />;
 }
