b3d3b41b4ea588e25d53d094a45302c7e894d03c
okamonster <okamonster0615@gmail.com>
2025-07-16 07:40:08 +0900

    feat: 検索機能を追加し、アーティスト名やイベント名での検索を可能にする

--- apps/web/src/features/top/components/UnloginHeroSection.tsx
+++ apps/web/src/features/top/components/UnloginHeroSection.tsx
@@ -1,7 +1,17 @@
+'use client';
 import { Button, Input } from '@mantine/core';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 export const UnloginHeroSection = (): React.ReactNode => {
+  const { push } = useRouter();
+  const [search, setSearch] = useState('');
+
+  const handleSearch = () => {
+    push(`/artists?query=${search}`);
+  };
+
   return (
     <div className="bg-gradient-to-r mx-auto px-4 from-pink-500 to-purple-600 py-16 grid gap-4 text-center">
       <h1 className="text-4xl font-bold text-text-white">
@@ -12,8 +22,14 @@ export const UnloginHeroSection = (): React.ReactNode => {
       <p className="text-md text-text-white">今すぐ確認！</p>
 
       <div className="flex items-center justify-center gap-2 w-full">
-        <Input radius="lg" w="250px" placeholder="アーティスト名やイベント名で検索" />
-        <Button radius="lg" color="var(--color-background-dark)" w="50px">
+        <Input
+          radius="lg"
+          w="250px"
+          placeholder="アーティスト名やイベント名で検索"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Button radius="lg" color="var(--color-background-dark)" w="50px" onClick={handleSearch}>
           <FaSearch />
         </Button>
       </div>
