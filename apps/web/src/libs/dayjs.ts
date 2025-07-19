0680a832b6ff519e056ad5fff486943bc66383bf
okamonster <okamonster0615@gmail.com>
2025-07-19 10:49:21 +0900

    fix: dayjsにタイムゾーンとUTCプラグインを追加し、日本時間をデフォルトタイムゾーンに設定

--- apps/web/src/libs/dayjs.ts
+++ apps/web/src/libs/dayjs.ts
@@ -1,11 +1,18 @@
 import dayjs from 'dayjs';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/ja';
 
 // dayjsプラグインを拡張
 dayjs.extend(isSameOrAfter);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 // 日本語ロケールを設定
 dayjs.locale('ja');
+
+// デフォルトタイムゾーンを日本時間に設定
+dayjs.tz.setDefault('Asia/Tokyo');
 
 export default dayjs;
