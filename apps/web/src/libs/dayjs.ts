eb27c29b32fddf438528e0c42b81e94dd92e567d
okamonster <okamonster0615@gmail.com>
2025-07-19 11:52:22 +0900

    fix: dayjsにUTCプラグインを追加し、タイムゾーン処理の拡張を行う

--- apps/web/src/libs/dayjs.ts
+++ apps/web/src/libs/dayjs.ts
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/ja';
 
 // dayjsプラグインを拡張
 dayjs.extend(isSameOrAfter);
+dayjs.extend(utc);
 dayjs.extend(timezone);
 
 // 日本語ロケールを設定
